c8ef27d897a7a104012b346826c40e1007ccafe7
Armin Novak <armin.novak@thincast.com>
2025-04-09 09:25:28 +0200

    add additional attributes

--- main.cpp
+++ main.cpp
@@ -5,9 +5,23 @@
 #include <string.h>
 #include <winscard.h>
 
+/*
+ * Tags for requesting card and reader attributes
+ */
+
 #define SCARD_ATTR_VALUE(Class, Tag) ((((ULONG)(Class)) << 16) | ((ULONG)(Tag)))
 
 #define SCARD_CLASS_VENDOR_INFO 1
+#define SCARD_CLASS_COMMUNICATIONS 2
+#define SCARD_CLASS_PROTOCOL 3
+#define SCARD_CLASS_POWER_MGMT 4
+#define SCARD_CLASS_SECURITY 5
+#define SCARD_CLASS_MECHANICAL 6
+#define SCARD_CLASS_VENDOR_DEFINED 7
+#define SCARD_CLASS_IFD_PROTOCOL 8
+#define SCARD_CLASS_ICC_STATE 9
+#define SCARD_CLASS_SYSTEM 0x7fff
+
 #define SCARD_ATTR_VENDOR_NAME SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_INFO, 0x0100)
 #define SCARD_ATTR_VENDOR_IFD_TYPE                                             \
   SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_INFO, 0x0101)
@@ -15,6 +29,77 @@
   SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_INFO, 0x0102)
 #define SCARD_ATTR_VENDOR_IFD_SERIAL_NO                                        \
   SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_INFO, 0x0103)
+#define SCARD_ATTR_CHANNEL_ID                                                  \
+  SCARD_ATTR_VALUE(SCARD_CLASS_COMMUNICATIONS, 0x0110)
+#define SCARD_ATTR_ASYNC_PROTOCOL_TYPES                                        \
+  SCARD_ATTR_VALUE(SCARD_CLASS_PROTOCOL, 0x0120)
+#define SCARD_ATTR_DEFAULT_CLK SCARD_ATTR_VALUE(SCARD_CLASS_PROTOCOL, 0x0121)
+#define SCARD_ATTR_MAX_CLK SCARD_ATTR_VALUE(SCARD_CLASS_PROTOCOL, 0x0122)
+#define SCARD_ATTR_DEFAULT_DATA_RATE                                           \
+  SCARD_ATTR_VALUE(SCARD_CLASS_PROTOCOL, 0x0123)
+#define SCARD_ATTR_MAX_DATA_RATE SCARD_ATTR_VALUE(SCARD_CLASS_PROTOCOL, 0x0124)
+#define SCARD_ATTR_MAX_IFSD SCARD_ATTR_VALUE(SCARD_CLASS_PROTOCOL, 0x0125)
+#define SCARD_ATTR_SYNC_PROTOCOL_TYPES                                         \
+  SCARD_ATTR_VALUE(SCARD_CLASS_PROTOCOL, 0x0126)
+#define SCARD_ATTR_POWER_MGMT_SUPPORT                                          \
+  SCARD_ATTR_VALUE(SCARD_CLASS_POWER_MGMT, 0x0131)
+#define SCARD_ATTR_USER_TO_CARD_AUTH_DEVICE                                    \
+  SCARD_ATTR_VALUE(SCARD_CLASS_SECURITY, 0x0140)
+#define SCARD_ATTR_USER_AUTH_INPUT_DEVICE                                      \
+  SCARD_ATTR_VALUE(SCARD_CLASS_SECURITY, 0x0142)
+#define SCARD_ATTR_CHARACTERISTICS                                             \
+  SCARD_ATTR_VALUE(SCARD_CLASS_MECHANICAL, 0x0150)
+
+#define SCARD_ATTR_CURRENT_PROTOCOL_TYPE                                       \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0201)
+#define SCARD_ATTR_CURRENT_CLK                                                 \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0202)
+#define SCARD_ATTR_CURRENT_F SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0203)
+#define SCARD_ATTR_CURRENT_D SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0204)
+#define SCARD_ATTR_CURRENT_N SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0205)
+#define SCARD_ATTR_CURRENT_W SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0206)
+#define SCARD_ATTR_CURRENT_IFSC                                                \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0207)
+#define SCARD_ATTR_CURRENT_IFSD                                                \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0208)
+#define SCARD_ATTR_CURRENT_BWT                                                 \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x0209)
+#define SCARD_ATTR_CURRENT_CWT                                                 \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x020a)
+#define SCARD_ATTR_CURRENT_EBC_ENCODING                                        \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x020b)
+#define SCARD_ATTR_EXTENDED_BWT                                                \
+  SCARD_ATTR_VALUE(SCARD_CLASS_IFD_PROTOCOL, 0x020c)
+
+#define SCARD_ATTR_ICC_PRESENCE SCARD_ATTR_VALUE(SCARD_CLASS_ICC_STATE, 0x0300)
+#define SCARD_ATTR_ICC_INTERFACE_STATUS                                        \
+  SCARD_ATTR_VALUE(SCARD_CLASS_ICC_STATE, 0x0301)
+#define SCARD_ATTR_CURRENT_IO_STATE                                            \
+  SCARD_ATTR_VALUE(SCARD_CLASS_ICC_STATE, 0x0302)
+#define SCARD_ATTR_ATR_STRING SCARD_ATTR_VALUE(SCARD_CLASS_ICC_STATE, 0x0303)
+#define SCARD_ATTR_ICC_TYPE_PER_ATR                                            \
+  SCARD_ATTR_VALUE(SCARD_CLASS_ICC_STATE, 0x0304)
+
+#define SCARD_ATTR_ESC_RESET                                                   \
+  SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_DEFINED, 0xA000)
+#define SCARD_ATTR_ESC_CANCEL                                                  \
+  SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_DEFINED, 0xA003)
+#define SCARD_ATTR_ESC_AUTHREQUEST                                             \
+  SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_DEFINED, 0xA005)
+#define SCARD_ATTR_MAXINPUT SCARD_ATTR_VALUE(SCARD_CLASS_VENDOR_DEFINED, 0xA007)
+
+#define SCARD_ATTR_DEVICE_UNIT SCARD_ATTR_VALUE(SCARD_CLASS_SYSTEM, 0x0001)
+#define SCARD_ATTR_DEVICE_IN_USE SCARD_ATTR_VALUE(SCARD_CLASS_SYSTEM, 0x0002)
+#define SCARD_ATTR_DEVICE_FRIENDLY_NAME_A                                      \
+  SCARD_ATTR_VALUE(SCARD_CLASS_SYSTEM, 0x0003)
+#define SCARD_ATTR_DEVICE_SYSTEM_NAME_A                                        \
+  SCARD_ATTR_VALUE(SCARD_CLASS_SYSTEM, 0x0004)
+#define SCARD_ATTR_DEVICE_FRIENDLY_NAME_W                                      \
+  SCARD_ATTR_VALUE(SCARD_CLASS_SYSTEM, 0x0005)
+#define SCARD_ATTR_DEVICE_SYSTEM_NAME_W                                        \
+  SCARD_ATTR_VALUE(SCARD_CLASS_SYSTEM, 0x0006)
+#define SCARD_ATTR_SUPRESS_T1_IFS_REQUEST                                      \
+  SCARD_ATTR_VALUE(SCARD_CLASS_SYSTEM, 0x0007)
 
 #if defined(_WIN32)
 #define SCARD_PROTOCOL_ANY (SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
@@ -134,49 +219,92 @@ public:
     }
 
     bool list() const {
-        const std::map<DWORD, std::string> attrs = {
-            {SCARD_ATTR_VENDOR_NAME, "SCARD_ATTR_VENDOR_NAME"},
-            {SCARD_ATTR_VENDOR_IFD_TYPE, "SCARD_ATTR_VENDOR_IFD_TYPE"},
-            {SCARD_ATTR_VENDOR_IFD_VERSION, "SCARD_ATTR_VENDOR_IFD_VERSION"},
-            {SCARD_ATTR_VENDOR_IFD_SERIAL_NO, "SCARD_ATTR_VENDOR_IFD_SERIAL_NO"}
-        };
+      const std::map<DWORD, std::string> attrs = {
+          {SCARD_ATTR_VENDOR_NAME, "SCARD_ATTR_VENDOR_NAME"},
+          {SCARD_ATTR_VENDOR_IFD_TYPE, "SCARD_ATTR_VENDOR_IFD_TYPE"},
+          {SCARD_ATTR_VENDOR_IFD_VERSION, "SCARD_ATTR_VENDOR_IFD_VERSION"},
+          {SCARD_ATTR_VENDOR_IFD_SERIAL_NO, "SCARD_ATTR_VENDOR_IFD_SERIAL_NO"},
+          {SCARD_ATTR_CHANNEL_ID, "SCARD_ATTR_CHANNEL_ID"},
+          {SCARD_ATTR_ASYNC_PROTOCOL_TYPES, "SCARD_ATTR_ASYNC_PROTOCOL_TYPES"},
+          {SCARD_ATTR_DEFAULT_CLK, "SCARD_ATTR_DEFAULT_CLK"},
+          {SCARD_ATTR_MAX_CLK, "SCARD_ATTR_MAX_CLK"},
+          {SCARD_ATTR_DEFAULT_DATA_RATE, "SCARD_ATTR_DEFAULT_DATA_RATE"},
+          {SCARD_ATTR_MAX_DATA_RATE, "SCARD_ATTR_MAX_DATA_RATE"},
+          {SCARD_ATTR_MAX_IFSD, "SCARD_ATTR_MAX_IFSD"},
+          {SCARD_ATTR_SYNC_PROTOCOL_TYPES, "SCARD_ATTR_SYNC_PROTOCOL_TYPES"},
+          {SCARD_ATTR_POWER_MGMT_SUPPORT, "SCARD_ATTR_POWER_MGMT_SUPPORT"},
+          {SCARD_ATTR_USER_TO_CARD_AUTH_DEVICE,
+           "SCARD_ATTR_USER_TO_CARD_AUTH_DEVICE"},
+          {SCARD_ATTR_USER_AUTH_INPUT_DEVICE,
+           "SCARD_ATTR_USER_AUTH_INPUT_DEVICE"},
+          {SCARD_ATTR_CHARACTERISTICS, "SCARD_ATTR_CHARACTERISTICS"},
+          {SCARD_ATTR_CURRENT_PROTOCOL_TYPE, "CARD_ATTR_CURRENT_PROTOCOL_TYPE"},
+          {SCARD_ATTR_CURRENT_CLK, "SCARD_ATTR_CURRENT_CLK"},
+          {SCARD_ATTR_CURRENT_F, "SCARD_ATTR_CURRENT_F"},
+          {SCARD_ATTR_CURRENT_D, "SCARD_ATTR_CURRENT_D"},
+          {SCARD_ATTR_CURRENT_N, "SCARD_ATTR_CURRENT_N"},
+          {SCARD_ATTR_CURRENT_W, "SCARD_ATTR_CURRENT_W"},
+          {SCARD_ATTR_CURRENT_IFSC, "SCARD_ATTR_CURRENT_IFSC"},
+          {SCARD_ATTR_CURRENT_IFSD, "SCARD_ATTR_CURRENT_IFSD"},
+          {SCARD_ATTR_CURRENT_BWT, "SCARD_ATTR_CURRENT_BWT"},
+          {SCARD_ATTR_CURRENT_CWT, "SCARD_ATTR_CURRENT_CWT"},
+          {SCARD_ATTR_CURRENT_EBC_ENCODING, "SCARD_ATTR_CURRENT_EBC_ENCODING"},
+          {SCARD_ATTR_EXTENDED_BWT, "SCARD_ATTR_EXTENDED_BWT"},
+          {SCARD_ATTR_ICC_PRESENCE, "SCARD_ATTR_ICC_PRESENCE"},
+          {SCARD_ATTR_ICC_INTERFACE_STATUS, "SCARD_ATTR_ICC_INTERFACE_STATUS"},
+          {SCARD_ATTR_CURRENT_IO_STATE, "SCARD_ATTR_CURRENT_IO_STATE"},
+          {SCARD_ATTR_ATR_STRING, "SCARD_ATTR_ATR_STRING"},
+          {SCARD_ATTR_ICC_TYPE_PER_ATR, "SCARD_ATTR_ICC_TYPE_PER_ATR"},
+          {SCARD_ATTR_ESC_RESET, "SCARD_ATTR_ESC_RESET SCARD"},
+          {SCARD_ATTR_ESC_CANCEL, "SCARD_ATTR_ESC_CANCEL"},
+          {SCARD_ATTR_ESC_AUTHREQUEST, "SCARD_ATTR_ESC_AUTHREQUEST"},
+          {SCARD_ATTR_MAXINPUT, "SCARD_ATTR_MAXINPUT"},
+          {SCARD_ATTR_DEVICE_UNIT, "SCARD_ATTR_DEVICE_UNIT"},
+          {SCARD_ATTR_DEVICE_IN_USE, "SCARD_ATTR_DEVICE_IN_USE"},
+          {SCARD_ATTR_DEVICE_FRIENDLY_NAME_A,
+           "SCARD_ATTR_DEVICE_FRIENDLY_NAME_A"},
+          {SCARD_ATTR_DEVICE_SYSTEM_NAME_A, "SCARD_ATTR_DEVICE_SYSTEM_NAME_A"},
+          {SCARD_ATTR_DEVICE_FRIENDLY_NAME_W,
+           "SCARD_ATTR_DEVICE_FRIENDLY_NAME_W"},
+          {SCARD_ATTR_DEVICE_SYSTEM_NAME_W, "SCARD_ATTR_DEVICE_SYSTEM_NAME_W"},
+          {SCARD_ATTR_SUPRESS_T1_IFS_REQUEST,
+           "SCARD_ATTR_SUPRESS_T1_IFS_REQUEST"}};
 
-        for (auto attr : attrs) {
-            DWORD len = 0;
-            std::string sattr;
-            if (_autoalloc) {
-              len = SCARD_AUTOALLOCATE;
-              char *str = nullptr;
-              auto rc = SCardGetAttrib(_card, attr.first,
-                                       reinterpret_cast<LPBYTE>(&str), &len);
-              std::cout << "SCardGetAttrib: (1): (autoalloc " << _autoalloc
-                        << ") " << attr.second << ": " << err2str(rc)
-                        << std::endl;
-              if (rc != SCARD_S_SUCCESS)
-                continue;
+      for (auto attr : attrs) {
+        DWORD len = 0;
+        std::string sattr;
+        if (_autoalloc) {
+          len = SCARD_AUTOALLOCATE;
+          char *str = nullptr;
+          auto rc = SCardGetAttrib(_card, attr.first,
+                                   reinterpret_cast<LPBYTE>(&str), &len);
+          std::cout << "SCardGetAttrib: (1): (autoalloc " << _autoalloc << ") "
+                    << attr.second << ": " << err2str(rc) << std::endl;
+          if (rc != SCARD_S_SUCCESS)
+            continue;
 
-              sattr = std::string(str, len);
-              SCardFreeMemory(_context, str);
-            } else {
-              auto rc = SCardGetAttrib(_card, attr.first, nullptr, &len);
-              std::cout << "SCardGetAttrib: (1): (autoalloc " << _autoalloc
-                        << ") " << attr.second << ": " << err2str(rc)
-                        << std::endl;
-              if (rc != SCARD_S_SUCCESS)
-                continue;
+          sattr = std::string(str, len);
+          SCardFreeMemory(_context, str);
+        } else {
+          auto rc = SCardGetAttrib(_card, attr.first, nullptr, &len);
+          std::cout << "SCardGetAttrib: (1): (autoalloc " << _autoalloc << ") "
+                    << attr.second << ": " << err2str(rc) << std::endl;
+          if (rc != SCARD_S_SUCCESS)
+            continue;
 
-            sattr.resize(len);
+          sattr.resize(len);
 
-            auto rc2 = SCardGetAttrib(_card, attr.first, reinterpret_cast<LPBYTE>(sattr.data()), &len);
-            std::cout << "SCardGetAttrib: (2): (autoalloc " << _autoalloc
-                      << ") " << rc2 << std::endl;
-            if (rc2 != SCARD_S_SUCCESS)
-                throw rc2;
-            }
-            std::cout << "SCardGetAttrib: (2): (autoalloc " << _autoalloc
-                      << ") " << attr.second << ": [" << len << "] {"
-                      << string_to_hex(sattr) << "} '" << sattr.c_str() << "'"
-                      << std::endl;
+          auto rc2 = SCardGetAttrib(
+              _card, attr.first, reinterpret_cast<LPBYTE>(sattr.data()), &len);
+          std::cout << "SCardGetAttrib: (2): (autoalloc " << _autoalloc << ") "
+                    << rc2 << std::endl;
+          if (rc2 != SCARD_S_SUCCESS)
+            throw rc2;
+        }
+        std::cout << "SCardGetAttrib: (2): (autoalloc " << _autoalloc << ") "
+                  << attr.second << ": [" << len << "] {"
+                  << string_to_hex(sattr) << "} '" << sattr.c_str() << "'"
+                  << std::endl;
         }
         return true;
     }
